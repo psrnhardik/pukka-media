9e94800f97bfe5bb59991a1b7c11deae7f5bac99
unknown <psrn.hardik@gmail.com>
2021-12-28 21:11:41 +0530

    20211228-2111

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,6 +14,7 @@
             bottom: 0;
             min-width: 100%;
             min-height: 100%;
+            background-position: center center;
         }
 
         .content {
@@ -45,16 +46,6 @@
             .main-image{
                 height: 270px !important;
                 margin-top: -105px !important;
-            }
-
-            .backgroundVideo {
-                position: fixed;
-                top: 0;
-                left: 0;
-                right: 0;
-                bottom: 0;
-                min-width: 100%;
-                min-height: 100%;
             }
         }
     </style>
